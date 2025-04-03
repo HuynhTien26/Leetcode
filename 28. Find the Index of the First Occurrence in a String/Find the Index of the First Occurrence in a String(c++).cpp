2474e8b6fa6d1f3358c518620ba1594624bcdda7
Huỳnh Thị Thủy Tiên <thtien.2606@gmail.com>
2025-04-03 18:52:22 +0700

    Update Find the Index of the First Occurrence in a String(c++).cpp

--- 28. Find the Index of the First Occurrence in a String/Find the Index of the First Occurrence in a String(c++).cpp
+++ 28. Find the Index of the First Occurrence in a String/Find the Index of the First Occurrence in a String(c++).cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-    if(needle.length()==0) return 0;
-    if(needle.length()>haystack.length()) return -1;
+    if(needle.length()==0) return 0; //needle rỗng
+    if(needle.length()>haystack.length()) return -1; //needle lớn hơn haystack
 
-    for(int i=0; i<= haystack.length() - needle.length(); i++){
-        if (haystack.substr(i, needle.length()) == needle){
-            return i;
+    for(int i=0; i<= haystack.length() - needle.length(); i++){ //duyệt haystack để tìm needlle
+        if (haystack.substr(i, needle.length()) == needle){ 
+            return i; //trả về vị trí đầu tiên khi tìm thấy needle
         }
      }
-     return -1;
+     return -1; //nếu không tìm thấy needle trong haystack
     }
 };
