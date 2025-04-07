ab1c810b6bd7981c9fa8fa91eab1b39b71822fce
Huỳnh Thị Thủy Tiên <thtien.2606@gmail.com>
2025-04-07 13:52:10 +0700

    Update Letter Combinations of a Phone Number(c++).cpp

--- 17. Letter Combinations of a Phone Number/Letter Combinations of a Phone Number(c++).cpp
+++ 17. Letter Combinations of a Phone Number/Letter Combinations of a Phone Number(c++).cpp
@@ -1,1 +1,26 @@
-**Đề bài: Cho một chuỗi chứa các chữ số từ bao gồm, trả về tất cả các tổ hợp chữ cái có thể có mà số đó có thể đại diện. Trả lại câu trả lời theo bất kỳ thứ tự nào.2-9**
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        if (digits.empty()) return {};
+                unordered_map<char, string> pad = {
+            {'2', "abc"}, {'3', "def"}, {'4', "ghi"}, {'5', "jkl"},
+            {'6', "mno"}, {'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"}
+        };
+    queue<string> q;
+    q.push("");
+    for(char digit :digits){
+        int size =q.size();
+        while (size--){
+            string cur=q.front();q.pop();
+            for(char letter : pad[digit]){
+                q.push(cur + letter);
+            }
+        }
+    }
+    vector<string> result;
+    while(!q.empty()){
+        result.push_back(q.front());q.pop();
+    }
+    return result;
+    }
+};
