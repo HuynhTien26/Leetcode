fba0a1771379738e1e273c738a3c4aace9c4a87f
Huỳnh Thị Thủy Tiên <thtien.2606@gmail.com>
2025-04-07 19:52:24 +0700

    Update Subtract the Product and Sum of Digits of an Integer(c++).cpp

--- 1281. Subtract the Product and Sum of Digits of an Integer/Subtract the Product and Sum of Digits of an Integer(c++).cpp
+++ 1281. Subtract the Product and Sum of Digits of an Integer/Subtract the Product and Sum of Digits of an Integer(c++).cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int subtractProductAndSum(int n) {
         int tong=0;
-        long long nhan=1;
+        int nhan=1;
         
-        while(n>0){
+        while(n){ //chạy tới khi n=0 thì dừng
             int digit =n%10;
             tong += digit;
             nhan *= digit;
