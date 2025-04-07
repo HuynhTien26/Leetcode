0b10561634c277dbb0a7cb104888053b53e0036d
Huỳnh Thị Thủy Tiên <thtien.2606@gmail.com>
2025-04-07 20:59:06 +0700

    Update Sqrt(x) (c++,1 line).cpp

--- 69. Sqrt(x)/Sqrt(x) (c++,1 line).cpp
+++ 69. Sqrt(x)/Sqrt(x) (c++,1 line).cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int mySqrt(int x) {
+        if(x<1) return 0;
         return sqrt(x); //sqrt trả về double (kiểu 2.0)
       //nhưng gặp mySqrt trên ở dạng int => c++ tự động ép kiểu về int
     }
